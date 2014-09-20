2b56ff193ecd04bd0e2c0418ccb232d37f7da2cb
Mateusz Palyz <m.palyz@pilot.co>
2014-09-20 18:31:18 +0200

    #2 add tests for 'from_usd', 'from_eur', 'from_gbp' class methods

--- money_test.rb
+++ money_test.rb
@@ -13,4 +13,16 @@ class MoneyTest < Minitest::Test
   def test_inspect_usage
     assert_equal "#<Money 10.00 USD>", @money.inspect
   end
+
+  def test_from_usd_usage
+    assert_equal "#<Money 10.00 USD>", Money.from_usd(10).inspect
+  end
+
+  def test_from_eur_usage
+    assert_equal "#<Money 10.00 EUR>", Money.from_eur(10).inspect
+  end
+
+  def test_from_gbp_usage
+    assert_equal "#<Money 10.00 GBP>", Money.from_gbp(10).inspect
+  end
 end
